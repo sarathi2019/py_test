1e72645b1b966cdedeeafea0a3f5990536c9bd2b
sarathi2019 <mycoursework2019@gmail.com>
2024-12-23 10:51:17 +0100

    update-2

--- source/my_build.py
+++ source/my_build.py
@@ -3,7 +3,7 @@ import os
 def open_command_window_and_greet():
     message = 'Hi, you are in the build stage'
     os.system(f'start cmd /k echo {message}')
-    print("hello build")
+    print("hello build -- inside the repo")
 
 if __name__ == "__main__":
     open_command_window_and_greet()
